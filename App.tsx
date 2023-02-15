435b9cdb410232e8a3c9a15e3669bb0e368ab603
Mehmet Cabbar <mehmet.cabbar@missafir.com>
2023-02-15 22:24:33 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <View>
+        <Text>My first commit</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
